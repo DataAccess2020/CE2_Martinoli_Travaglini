0b37fd70a00825d388f8434cad68fb292117a885
simonetravaglini1 <simonetravaglini@studenti.unimi.it>
2022-12-02 13:00:12 +0100

    i delete the previous function and change it with xml

--- scripts/CE2_script2_getlinks.R
+++ scripts/CE2_script2_getlinks.R
@@ -1,12 +1,18 @@
 library(rvest)
-library(tidyverse)
 
-#get all links from the page
 
-links <- rvest::read_html(resp) %>% 
-  html_elements(css = "a") %>% 
-  html_text()
+#get all links from the page-----
 
+install.packages("XML")
+library(XML)
+
+resp
+links <- XML::getHTMLLinks(resp, externalOnly = T)
 links
 
-#error: I can see only the title of the links, not the actual link
+library(tidyverse)
+#create a table with all the links extracts----
+data <- tibble(links)
+
+
+# i fix the problem with the function gethtmllinks by xml, insted of use rvest.
