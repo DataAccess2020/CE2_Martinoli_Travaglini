acfd0a129bfbd9e138e66a12b36315804e87577d
simonetravaglini1 <simonetravaglini@studenti.unimi.it>
2022-12-03 15:48:20 +0100

    extract only the articles link

--- scripts/CE2_script3_extractbloglinks.R
+++ scripts/CE2_script3_extractbloglinks.R
@@ -4,22 +4,32 @@ library(dplyr)
 
 #in order to match the links i use the function str_view_all
 
-str_view_all(links, pattern = "^https://beppegrillo\\.it/.+")
-
-str_view_all(links, pattern = "https://beppegrillo\\.it/grilloteca")
+str_view_all(links, pattern = "^https://beppegrillo\\.it/.+-.+-")
   
 #extract all the links of the beppe grillo blog, 
 #and then put it in a table called "links_bgrillo"
 
 links_bgrillo_list <- str_extract_all(
   links, 
-  pattern = "^https://beppegrillo\\.it/.+")
+  pattern = "^https://beppegrillo\\.it/.+-.+-.+")
 
 links_unl <- unlist(links_bgrillo_list)
 links_bgrillo <- as.data.frame(links_unl)
 
 
 links_unl == links_bgrillo
+
+links_bgrillo_list
+
+#tryng to extract only the articles liks
+str_view_all(links_unl, pattern = "^https://beppegrillo\\.it/[^category].+")
+
+link_grillo str_extract_all(
+  links_unl, pattern = "^https://beppegrillo\\.it/[^category].+")
+
+
+
+
 
 
 
